55815f5602204f6026830e1b74e3a11e280f9568
Abdul Wahid <111135216+abdulwahid4423@users.noreply.github.com>
2022-10-21 06:40:31 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+
+
+var min = 0;
+var sec = 0;
+var msec = 0;
+
+var minHeading = document.getElementById("min")
+var secHeading = document.getElementById("sec")
+var msecHeading = document.getElementById("msec")
+var interval;
+function timer(){
+    msec++
+    msecHeading.innerHTML = msec
+    if(msec>= 100){
+        sec++
+        secHeading.innerHTML = sec;
+      msec = 0;  
+    }else if(sec>= 60){
+        min++
+        minHeading.innerHTML = min;
+        sec = 0;
+    }
+} 
+function start(){
+interval = setInterval( timer,10)
+var btn = document.getElementById("btn")
+btn.disabled = true;
+var btn = document.getElementById("pause")
+pause.disabled= false;
+var btn = document.getElementById("restart")
+    Restart.disabled = false;
+}
+
+function stop(){
+    clearInterval(interval)
+    var btn = document.getElementById("pause")
+    pause.disabled= true;
+    var btn = document.getElementById("btn")
+btn.disabled = false;
+
+}
+
+function reset(){
+    min = 0; 
+    sec = 0; 
+    msec = 0;
+    minHeading.innerHTML = min 
+    secHeading.innerHTML = sec 
+    msecHeading.innerHTML = msec
+    stop()
+    var btn = document.getElementById("restart")
+    Restart.disabled = true;
+    var btn = document.getElementById("btn")
+    btn.disabled = false;
+    var btn = document.getElementById("pause")
+    pause.disabled= true;
+ 
+}
+
+
+
+
+
+
+
+
+
+
